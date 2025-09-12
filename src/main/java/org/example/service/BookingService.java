a40b8f2accb480e9f44e8e1f10262707f549ff77
Sumit Negi <sumitnegi@Sumits-MacBook-Pro.local>
2025-09-12 22:06:01 +0530

    feat : Booking service logic updated

--- src/main/java/org/example/service/BookingService.java
+++ src/main/java/org/example/service/BookingService.java
@@ -1,13 +1,24 @@
 package org.example.service;
 
 import org.example.dao.AvailableTableDAO;
+import org.example.dao.BookingDAO;
 
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class BookingService {
     Scanner input = new Scanner(System.in);
+    int generateFourDigitCode(){
+        int randomNumber = 0;
+        Random random = new Random();
+        for(int curDigit = 1;curDigit<=4;curDigit++){
+            randomNumber = randomNumber*10+random.nextInt(10);
+        }
+        return randomNumber;
+    }
     void bookTable(int userId){
+        BookingDAO bookingDAO = new BookingDAO();
         Scanner input = new Scanner(System.in);
         System.out.println("Let me check the availability");
         AvailableTableDAO availableTableDAO = new AvailableTableDAO();
@@ -16,14 +27,14 @@ public class BookingService {
             System.out.println("No tables are available right now please wait and try booking after 5-10 minutes later");
             return;
         }
-        else{
-            System.out.println("Tables are available please type 'yes' to book a table");
-            String userDecision = input.nextLine();
-            if(!Objects.equals(userDecision, "yes"))return;
-            System.out.println("Your table has been booked please visit the restaurant within 15 minutes else your booking would be canceled");
+
+        System.out.println("Tables are available please type 'yes' to book a table");
+        String userDecision = input.nextLine();
+        if(!Objects.equals(userDecision, "yes"))return;
+        System.out.println("Your table has been booked please visit the restaurant within 15 minutes else your booking would be canceled");
+
+        bookingDAO.insertBooking(1,generateFourDigitCode());
 
 
-
-        }
     }
 }
